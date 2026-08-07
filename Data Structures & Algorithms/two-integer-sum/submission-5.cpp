4b551ecc25ecb9e969a87b775d95529b44f92a7b
Toni Blair <147133152+toni8283@users.noreply.github.com>
2026-08-07 15:30:43 +0530

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> arrmap;
+        arrmap.reserve(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            int reqno = target - nums[i];
+
+            auto it = arrmap.find(reqno);
+            if (it != arrmap.end()) {
+                return {it->second, i};
+            }
+            
+            arrmap[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
