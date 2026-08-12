62d0d0707a258366dcd7cce8e564121a673d0c32
Toni Blair <147133152+toni8283@users.noreply.github.com>
2026-08-12 23:29:48 +0530

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < numbers.size(); i++) {
+            int needed = target - numbers[i];
+
+            if (mp.find(needed) != mp.end()) {
+                return {mp[needed]+1, i+1};
+            }
+
+            mp[numbers[i]] = i;
+        }
+
+        return {};
+    }
+};
