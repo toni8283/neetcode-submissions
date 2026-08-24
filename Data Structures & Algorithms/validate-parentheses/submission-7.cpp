d1ab5c36bcc41cecf13a5228254806632b3ca3b0
Toni Blair <147133152+toni8283@users.noreply.github.com>
2026-08-24 11:25:51 +0530

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int l = s.length();
+        /*if (l % 2 != 0) {
+            return false;
+        }*/
+        stack<char> st;
+
+        for (char c : s) {
+
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                if (c - top == 1 || c - top == 2) {
+                    st.pop();
+                } else return false;
+            }
+            
+
+        }
+        return st.empty();
+
+
+        
+    }
+};
